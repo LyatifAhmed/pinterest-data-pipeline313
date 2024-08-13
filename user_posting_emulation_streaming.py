f1b9ba27fab85e2cba8a52e62d0f76bd43b648c8
LyatifAhmed <latifmutlu@gmail.com>
2024-08-13 18:59:55 +0100

    updates streaming.py file

--- user_posting_emulation_streaming.py
+++ user_posting_emulation_streaming.py
@@ -7,15 +7,10 @@ import json
 import sqlalchemy
 from sqlalchemy import text
 import yaml
+import datetime
 
 
 random.seed(100)
-
-#Adding the invoke_url for different topics
-# invoke url for one record, if you want to put more records replace record with records
-invoke_url_pin = "https://kpo94dagq0.execute-api.us-east-1.amazonaws.com/test/streams/streaming-0affcdd81315-pin/record"
-invoke_url_geo = "https://kpo94dagq0.execute-api.us-east-1.amazonaws.com/test/streams/streaming-0affcdd81315-geo/record"
-invoke_url_user = "https://kpo94dagq0.execute-api.us-east-1.amazonaws.com/test/streams/streaming-0affcdd81315-user/record"
 
 class AWSDBConnector:
 
@@ -70,55 +65,60 @@ def run_infinite_post_data_loop():
             #print(geo_result)
             #print(user_result)
 
-            # Payload for each pin, geo and user information from pinterest
-            payload_pin = json.dumps({
-                    "StreamName": "streaming-0affcdd81315-pin",
-                    "Data": {
-                            #Data should be send as pairs of column_name:value, with different columns separated by commas      
-                            "index": pin_result["index"], "unique_id": pin_result["unique_id"], "title": pin_result["title"], "description": pin_result["description"],
-                                       "poster_name": pin_result["poster_name"], "follower_count": pin_result["follower_count"], "tag_list": pin_result["tag_list"], "is_image_or_video": pin_result["is_image_or_video"], 
-                                       "image_src": pin_result["image_src"], "downloaded": pin_result["downloaded"], "save_location": pin_result["save_location"], "category": pin_result["category"]
-                            },
-                            "PartitionKey": "0affcdd81315-pin"
-                            })
-            
-            payload_geo = json.dumps({
-                    "StreamName": "streaming-0affcdd81315-geo",
-                    "Data": {
-                            #Data should be send as pairs of column_name:value, with different columns separated by commas      
-                            "ind": geo_result["ind"], "timestamp": geo_result["timestamp"], "latitude": geo_result["latitude"], "longitude": geo_result["longitude"], 
-                            "country": geo_result["country"]
-                            },
-                            "PartitionKey": "0affcdd81315-geo"
-                            }, default=str)
-            
+            invoke_url1 = "https://kpo94dagq0.execute-api.us-east-1.amazonaws.com/test/streams/streaming-0affcdd81315-pin/record"
 
-            payload_user = json.dumps({
-                    "StreamName": "streaming-0affcdd81315-user",
-                    "Data": {
-                            #Data should be send as pairs of column_name:value, with different columns separated by commas      
-                            "ind": user_result["ind"], "first_name": user_result["first_name"], "last_name": user_result["last_name"], "age": user_result["age"], "date_joined": user_result["date_joined"]
-                            },
-                            "PartitionKey": "0affcdd81315-user"
-                            }, default=str)
+            #To send JSON messages
+            payload1 = json.dumps({
+                "StreamName": "streaming-0affcdd81315-pin",
+                "Data": {
+                        #Data should be send as pairs of column_name:value, with different columns separated by commas      
+                        "index": pin_result["index"], "unique_id": pin_result["unique_id"], "title": pin_result["title"], "description": pin_result["description"], "poster_name": pin_result["poster_name"], 
+       "follower_count": pin_result["follower_count"], "tag_list": pin_result["tag_list"], "is_image_or_video": pin_result["is_image_or_video"], "image_src": pin_result["image_src"], "downloaded": pin_result["downloaded"], 
+       "save_location": pin_result["save_location"], "category": pin_result["category"]
+                        },
+                        "PartitionKey": "partition=1"
+                        })
 
+
+            invoke_url2 = "https://kpo94dagq0.execute-api.us-east-1.amazonaws.com/test/streams/streaming-0affcdd81315-geo/record"
+
+            #To send JSON messages
+            payload2 = json.dumps({
+                "StreamName": "streaming-0affcdd81315-geo",
+                "Data": {
+                        #Data should be send as pairs of column_name:value, with different columns separated by commas      
+                        "ind": geo_result["ind"], "timestamp": geo_result["timestamp"].strftime("%Y-%m-%d %H:%M:%S"), "latitude": geo_result["latitude"], "longitude": geo_result["longitude"], "country": geo_result["country"]
+                        },
+                        "PartitionKey": "partition=1"
+                        })
+
+
+            invoke_url3 = "https://kpo94dagq0.execute-api.us-east-1.amazonaws.com/test/streams/streaming-0affcdd81315-user/record"
             
-            #creating a header type for response
+            #To send JSON messages
+            payload3 = json.dumps({
+                "StreamName": "streaming-0affcdd81315-user",
+                "Data": {
+                        #Data should be send as pairs of column_name:value, with different columns separated by commas      
+                        "ind": user_result["ind"], "first_name": user_result["first_name"], "last_name": user_result["last_name"], "age": user_result["age"], "date_joined": user_result["date_joined"].strftime("%Y-%m-%d %H:%M:%S")
+                        },
+                        "PartitionKey": "partition=1"
+                        })
+
+
             headers = {'Content-Type': 'application/json'}
-            
-            #Creating individual request for the reponse
-            response_pin = requests.request("PUT", invoke_url_pin, headers=headers, data=payload_pin)
-            response_geo = requests.request("PUT", invoke_url_geo, headers=headers, data=payload_geo)
-            response_user = requests.request("PUT", invoke_url_user, headers=headers, data=payload_user)
-            
-            print(response_pin.status_code)
-            print(response_geo.status_code)
-            print(response_user.status_code)
+
+            response1 = requests.request("PUT", invoke_url1, headers=headers, data=payload1)
+            response2 = requests.request("PUT", invoke_url2, headers=headers, data=payload2)
+            response3 = requests.request("PUT", invoke_url3, headers=headers, data=payload3)
+            print(response1.status_code)
+            print(response2.status_code)
+            print(response3.status_code)
 
 
 if __name__ == "__main__":
     run_infinite_post_data_loop()
-    print('Working')
+    #print('Working')
     
     
 
